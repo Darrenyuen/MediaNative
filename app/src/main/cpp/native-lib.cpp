e243ba4723fdd6549d927fed6612aa580b6a718c
yuansssf@gmail.com <yuansssf@gmail.com>
2022-06-18 22:28:51 +0800

    update compileSdk to 31

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,6 +5,6 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_medianative_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello from CPP";
     return env->NewStringUTF(hello.c_str());
 }
